0a6ea4f2b02190597add8ad24fd33e03044a1c01
Moses-b <zimaymoses@gmail.com>
2026-04-03 17:28:48 +0200

    Avoid staticfiles warning when /static is missing

--- zimayblog/settings.py
+++ zimayblog/settings.py
@@ -181,7 +181,7 @@ USE_TZ = True
 
 STATIC_URL = "/static/"
 STATIC_ROOT = BASE_DIR / "staticfiles"
-STATICFILES_DIRS = [BASE_DIR / "static"]
+STATICFILES_DIRS = [BASE_DIR / "static"] if (BASE_DIR / "static").exists() else []
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
